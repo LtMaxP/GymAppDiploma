c685b58b195df37fd3c9bd766f8eda2766dd3795
LtMaxP <gonzalodebrito@hotmail.com>
2022-07-13 18:35:54 -0300

    Seccion Producto casi terminado

--- UI/Negocio/Productos.cs
+++ UI/Negocio/Productos.cs
@@ -16,7 +16,6 @@ namespace UI.Negocio
     {
         BLL.Negocio.BLLProducto BLLProd = new BLL.Negocio.BLLProducto();
         List<Item> items = new List<Item>();
-
         public Productos()
         {
             InitializeComponent();
@@ -44,16 +43,17 @@ namespace UI.Negocio
         }
         private void GenerarGrilla()
         {
-            dataGridView1.Columns.Add("ProductoCol", "Producto");
-            dataGridView1.Columns.Add("CantidadCol", "Cantidad");
-            dataGridView1.Columns.Add("PrecioCol", "Precio");
+            dataGridView1.Columns.Add("Producto", "Producto");
+            dataGridView1.Columns.Add("Cantidad", "Cantidad");
+            dataGridView1.Columns.Add("Precio", "Precio");
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string selectioncmb = ((Item)comboBox1.SelectedItem).Descripcion;
             if (!String.IsNullOrEmpty(comboBox1.Text))
             {
-                var item = items.FirstOrDefault(x => x.Descripcion == comboBox1.Text);
+                var item = items.First(x => x.Descripcion == selectioncmb);
                 labelNroDisponible.Text = item.Cantidad.ToString();
                 labelNroPrecio.Text = item.Valor.ToString();
             }
@@ -67,9 +67,31 @@ namespace UI.Negocio
         {
             if (!String.IsNullOrEmpty(txtBoxCantidad.Text))
             {
-                dataGridView1.Rows.Add(comboBox1.Text, txtBoxCantidad.Text, labelNroPrecio.Text);
+                string selectioncmb = ((Item)comboBox1.SelectedItem).Descripcion;
+                Boolean found = false;
+                try
+                {
+                    if (dataGridView1.Rows.Count > 1)
+                    {
+                        foreach (DataGridViewRow dgr in dataGridView1.Rows)
+                        {
+                            if (!dgr.Index.Equals(dataGridView1.Rows.Count - 1))
+                                if (dgr.Cells["Producto"].Value.ToString().Equals(selectioncmb))
+                                {
+                                    dgr.Cells["Cantidad"].Value = (Decimal.Parse(dgr.Cells["Cantidad"].Value.ToString()) + Decimal.Parse(txtBoxCantidad.Text)).ToString();
+                                    found = true;
+                                    break;
+                                }
+                        }
+                    }
+                }
+                catch { }
+                if (!found)
+                {
+                    dataGridView1.Rows.Add(comboBox1.Text, txtBoxCantidad.Text, labelNroPrecio.Text);
+                }
 
-                labelNroTotal.Text = ((Decimal.Parse(txtBoxCantidad.Text) * Decimal.Parse(labelNroPrecio.Text)) + Decimal.Parse(labelNroTotal.Text)).ToString();
+                CalcularValorTotal();
             }
             else
             {
@@ -77,14 +99,31 @@ namespace UI.Negocio
             }
         }
 
+        private void CalcularValorTotal()
+        {
+            Decimal total = Decimal.Parse(labelNroTotal.Text);
+            try
+            {
+                foreach (DataGridViewRow dgr in dataGridView1.Rows)
+                {
+                    if (!dgr.Index.Equals(0) && !dgr.Index.Equals(dataGridView1.Rows.Count - 1))
+                    {
+                        total += (Decimal.Parse(dgr.Cells["Cantidad"].Value.ToString()) * Decimal.Parse(dgr.Cells["Precio"].Value.ToString()));
+                    }
+                }
+            }
+            catch { }
+            labelNroTotal.Text = total.ToString();
+        }
+
         private void buttonQuitar_Click(object sender, EventArgs e)
         {
             if (dataGridView1.SelectedRows.Count > 0)
             {
-                //////////////////
-                foreach (int row in dataGridView1.SelectedRows)
+                foreach (DataGridViewRow row in dataGridView1.SelectedRows)
                 {
-                    dataGridView1.Rows.RemoveAt(row);
+                    labelNroTotal.Text = (decimal.Parse(labelNroTotal.Text) - (decimal.Parse(row.Cells[1].Value.ToString()) * decimal.Parse(row.Cells[2].Value.ToString()))).ToString();
+                    dataGridView1.Rows.Remove(row);
                 }
             }
         }
