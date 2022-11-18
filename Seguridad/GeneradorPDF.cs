4fea441fad681b63a890a9a54915582085f946c3
gdebrito <gdebrito@mvfactory.com>
2022-11-18 00:56:22 -0300

    PDF a capara servicios

--- Seguridad/GeneradorPDF.cs
+++ Seguridad/GeneradorPDF.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,60 @@ namespace Servicios
 {
     public class GeneradorPDF
     {
-        
+        public static bool GenerarFactura(string rooTFoolder, BE.BE_Factura factura)
+        {
+            bool returnable = false;
+            try
+            {
+
+                FileStream pdfroot = new FileStream(rooTFoolder, FileMode.Create);
+                Document doc = new Document(PageSize.LETTER, 5, 5, 7, 7);
+                PdfWriter pw = PdfWriter.GetInstance(doc, pdfroot);
+
+                doc.Open();
+                doc.AddAuthor("TechExtreme");
+                doc.AddTitle("PDF Generado");
+                iTextSharp.text.Font standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+                doc.Add(new Paragraph("Factura del documento"));
+                doc.Add(Chunk.NEWLINE);
+
+                PdfPTable tblPdf = new PdfPTable(3);
+                tblPdf.WidthPercentage = 100;
+
+                PdfPCell clProducto = new PdfPCell(new Phrase("Producto", standardFont));
+                clProducto.BorderWidth = 0;
+                clProducto.BorderWidthBottom = 0.75f;
+                PdfPCell clPrecio = new PdfPCell(new Phrase("Precio", standardFont));
+                clPrecio.BorderWidth = 0;
+                clPrecio.BorderWidthBottom = 0.75f;
+                PdfPCell clCantidad = new PdfPCell(new Phrase("Cantidad", standardFont));
+                clCantidad.BorderWidth = 0;
+                clCantidad.BorderWidthBottom = 0.75f;
+
+                foreach (BE.Item Item in factura.Items)
+                {
+                    clProducto = new PdfPCell(new Phrase(Item.Descripcion, standardFont));
+                    clProducto.BorderWidth = 0;
+
+                    clPrecio = new PdfPCell(new Phrase(Item.Valor.ToString(), standardFont));
+                    clPrecio.BorderWidth = 0;
+
+                    clCantidad = new PdfPCell(new Phrase(Item.Cantidad.ToString(), standardFont));
+                    clCantidad.BorderWidth = 0;
+
+                    tblPdf.AddCell(clProducto);
+                    tblPdf.AddCell(clPrecio);
+                    tblPdf.AddCell(clCantidad);
+                }
+
+                doc.Add(tblPdf);
+                returnable = true;
+                doc.Close();
+                pw.Close();
+            }
+            catch
+            { }
+            return returnable;
+        }
     }
 }
--- UI/Negocio/Facturas.cs
+++ UI/Negocio/Facturas.cs
@@ -109,59 +109,16 @@ namespace UI
             {
                 BE.BE_Factura factura = (BE.BE_Factura)dataGridViewFactura.SelectedRows[0].DataBoundItem;
                 factura = bllFacturas.TraerFacturaConItems(factura);
-                //pdf factu
                 FolderBrowserDialog folderDlg = new FolderBrowserDialog();
                 folderDlg.ShowNewFolderButton = true;
                 DialogResult result = folderDlg.ShowDialog();
                 if (result == DialogResult.OK)
                 {
                     string rooTFoolder = folderDlg.SelectedPath;
-                    //aca a la clase Servicios?
-                    FileStream pdfroot = new FileStream(rooTFoolder, FileMode.Create);
-                    Document doc = new Document(PageSize.LETTER, 5, 5, 7, 7);
-                    PdfWriter pw = PdfWriter.GetInstance(doc, pdfroot);
-
-                    doc.Open();
-                    doc.AddAuthor("WiredBox");
-                    doc.AddTitle("PDF Generado");
-                    iTextSharp.text.Font standardFont = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
-                    doc.Add(new Paragraph("Titulo del documento"));
-                    doc.Add(Chunk.NEWLINE);
-
-                    PdfPTable tblPdf = new PdfPTable(3);
-                    tblPdf.WidthPercentage = 100;
-
-                    PdfPCell clProducto = new PdfPCell(new Phrase("Producto", standardFont));
-                    clProducto.BorderWidth = 0;
-                    clProducto.BorderWidthBottom = 0.75f;
-                    PdfPCell clPrecio = new PdfPCell(new Phrase("Precio", standardFont));
-                    clPrecio.BorderWidth = 0;
-                    clPrecio.BorderWidthBottom = 0.75f;
-                    PdfPCell clCantidad = new PdfPCell(new Phrase("Cantidad", standardFont));
-                    clCantidad.BorderWidth = 0;
-                    clCantidad.BorderWidthBottom = 0.75f;
-
-                    foreach (BE.Item Item in factura.Items)
-                    {
-                        clProducto = new PdfPCell(new Phrase(Item.Descripcion, standardFont));
-                        clProducto.BorderWidth = 0;
-
-                        clPrecio = new PdfPCell(new Phrase(Item.Valor.ToString(), standardFont));
-                        clPrecio.BorderWidth = 0;
-
-                        clCantidad = new PdfPCell(new Phrase(Item.Cantidad.ToString(), standardFont));
-                        clCantidad.BorderWidth = 0;
-
-                        tblPdf.AddCell(clProducto);
-                        tblPdf.AddCell(clPrecio);
-                        tblPdf.AddCell(clCantidad);
-                    }
-
-                    doc.Add(tblPdf);
-                    doc.Close();
-
-                    pw.Close();
-                    MessageBox.Show("Exportación realizada con exito");
+                    if (Servicios.GeneradorPDF.GenerarFactura(rooTFoolder, factura))
+                        MessageBox.Show("Exportación realizada con exito");
+                    else
+                        MessageBox.Show("Error en la exportación");
                 }
             }
         }
